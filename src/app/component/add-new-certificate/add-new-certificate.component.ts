56af0d6467a2b7134f5bd75ed5cb7e07ada1bf8b
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2023-06-12 13:48:44 +0300

    #03 Mat-paginator + add new button

---
+++ src/app/component/add-new-certificate/add-new-certificate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-certificate',
+  templateUrl: './add-new-certificate.component.html',
+  styleUrls: ['./add-new-certificate.component.scss']
+})
+export class AddNewCertificateComponent {
+
+}
